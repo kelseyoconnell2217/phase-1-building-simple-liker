434934b0c21ab0b95148ea06d8cb44745a28fe8f
Kelsey OConnell <kelseyoconnell2217@gmail.com>
2024-02-15 22:33:13 -0500

    Pat on the Back!

--- main.js
+++ main.js
@@ -2,8 +2,39 @@
 const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
 
+function addListenerToButtons(){
+  const likeButtons = document.querySelectorAll("span.like-glyph")
+  for(let btn of likeButtons){
+    btn.addEventListener('click', handleLike)
+    }
+}
+
+addListenerToButtons()
+
+function handleLike(e){
+  let heart = e.target
+  mimicServerCall()  
+  .then(resp => {
+    if (resp){changeLike(heart)}})
+  .catch(error => {
+    console.log(error)
+    const modal = document.querySelector("#modal")
+    modal.className = ""
+    setTimeout(() => modal.className = "hidden", 3000)
+  })
+      
+
+}
+function changeLike(heart){
+  if (heart.innerText === EMPTY_HEART){
+    heart.innerText = FULL_HEART;
+    heart.className = 'activated-heart'}
+  else if (heart.innerText === FULL_HEART){
+    heart.innerText = EMPTY_HEART;
+    heart.className = ''}
+    else console.log("This isn't working!")
+}
 
 
 
